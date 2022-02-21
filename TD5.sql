3a3d45ba07bd44943bd6a8dcffa91bfe0e73cd0e
Clément Oiry <clement.oiry@etu.umontpellier.fr>
2022-02-21 12:00:08 +0100

    td5

--- TD5.sql
+++ TD5.sql
@@ -63,4 +63,68 @@ SELECT nomBungalow FROM Bungalows b
 
 --R73
 
+--division
 
+SELECT nomClient FROM Clients c
+	JOIN Locations l ON c.idClient = l.idClient
+	JOIN Bungalows b ON l.idBungalow = b.idBungalow
+	GROUP BY nomClient
+	HAVING COUNT(DISTINCT idCamping) = (SELECT COUNT (DISTINCT idCamping) FROM Bungalows);
+
+--division not exists(a réesayer)
+
+SELECT nomClient FROM Clients c
+	WHERE NOT EXISTS (SELECT l.idBungalow FROM Locations l
+					JOIN Bungalows b ON l.idBungalow = b.idBungalow
+					JOIN Campings camping ON b.idCamping = camping.idCamping
+					MINUS 
+					SELECT idBungalow FROM Locations l
+					WHERE b.idCamping = camping.idCamping)
+
+--R74
+
+
+SELECT nomClient FROM Clients c
+	WHERE NOT EXISTS (SELECT idBungalow FROM Locations l
+		JOIN Clients c ON l.idClient = c.idClient
+		WHERE nomClient ='Zeblouse'
+			AND prenomClient='Agathe'
+		MINUS
+		SELECT idBungalow FROM Locations l
+			WHERE l.idClient = c.idClient);
+
+--R75
+
+SELECT nomClient, prenomClient FROM Clients c
+	WHERE NOT EXISTS (SELECT idCamping FROM Bungalows b
+		JOIN Locations l ON b.idBungalow = l.idBungalow
+		JOIN Clients c ON l.idClient = c.idClient
+			WHERE nomClient ='Zeblouse'
+			 AND prenomClient ='Agathe'
+		MINUS 
+		SELECT idCamping FROM Bungalows b
+		JOIN Locations l ON b.idBungalow = l.idBungalow
+			WHERE c.idClient=l.idClient)
+	AND NOT EXISTS (SELECT idCamping FROM Bungalows b
+		JOIN Locations l ON b.idBungalow = l.idBungalow
+			WHERE c.idClient = l.idClient
+		MINUS 
+		SELECT idCamping FROM Bungalows b
+		JOIN Locations l ON b.idBungalow = l.idBungalow
+		JOIN Clients c ON l.idClient = c.idClient
+			WHERE nomClient ='Zeblouse'
+			 AND prenomClient ='Agathe');
+
+--R80
+
+
+SELECT idClient, nomClient, prenomClient FROM Clients c
+	WHERE idClient NOT IN (SELECT idClient FROM Locations l
+		JOIN Bungalows b ON l.idBungalow = b.idBungalow
+		JOIN Campings camp ON b.idCamping = camp.idCamping
+		WHERE villeCamping = 'Palavas');
+
+--R81
+
+
+			
