1afa33cb0b6d5371846bd7ccde4f20ab125a1260
Clément Oiry <clement.oiry@etu.umontpellier.fr>
2022-03-14 12:00:23 +0100

    Partie 4

--- TD6/td6.sql
+++ TD6/td6.sql
@@ -48,7 +48,7 @@ CREATE VIEW EmployesSansCamping(idEmploye, nomEmploye, prenomEmploye, salaireEmp
 
 SELECT * FROM EmployesSansCamping;
 
-INSERT INTO EmployesSansCamping (idEmploye, nomEmploye, prenomEmploye, salaireEmploye) VALUES (E100, Stiko, Judas, 3000);
+INSERT INTO EmployesSansCamping (idEmploye, nomEmploye, prenomEmploye, salaireEmploye) VALUES ('E100', Stiko, Judas, 3000);
 
 UPDATE EmployesSansCamping SET nomEmploye = 'Nana' WHERE idEmploye='E100';
 
@@ -92,3 +92,164 @@ CREATE VIEW BungalowsEtCampings (idBungalow, nomBungalow, superficieBungalow, id
 	SELECT idBungalow, nomBungalow, superficieBungalow, b.idCamping, nomCamping
 	FROM Bungalows b
 	JOIN Campings c ON b.idCamping =c.idCamping;
+
+-- Impossible d'insérer des lignes simultanéments dans les tables Bungalows et Campings car la table Camping
+--n'est pas protégé par sa clé
+--Oui il est possible d'ajouter/modifier/supprimer des lignes dans la table Bungalows car elle est protégé par sa clé (clé primaire est préserversé dans la clause de jointure)
+--Impossible d'insérer/modifier/supprimer  des lignes dans la table Campings car non protégé par sa clé
+
+
+INSERT INTO BungalowsEtCampings VALUES ('B13', 'Le Souterrain', 75, 'CAMP10', 'Yellow Shark');
+
+INSERT INTO BungalowsEtCampings(idBungalow, nomBungalow, superficieBungalow) VALUES ('B14', 'Le Dépotoir', 25);
+
+INSERT INTO BungalowsEtCampings (idCamping, nomCamping) VALUES ('CAMP11', 'Apelsin Mollusk');
+
+UPDATE BungalowsEtCampings SET superficieBungalow = 99 WHERE nomBungalow ='Le Palace';
+
+UPDATE BungalowsEtCampings SET nomCamping = 'Le Majestique Blanc' WHERE nomCamping = 'The White Majestic';
+
+--7
+
+CREATE VIEW CampingsPalavas (idCamping, nomCamping, villeCamping, nbEtoilesCamping) AS
+	SELECT idCamping, nomCamping, villeCamping, nbEtoilesCamping
+	FROM Campings
+	WHERE villeCamping = 'Palavas'
+	
+
+--Possible dans les deux cas, quand le camping n'est pas dans la ville Palavas, l'ajout se fait dans le table Campings et non dans la vue
+
+INSERT INTO CampingsPalavas VALUES ('CAMP4', 'El Delfin Azul', 'Carnon', 3);
+
+
+--Modification de la vue pour qu'on ne puisse insérer que des campings qui se trouvent dans la ville Palavas
+
+CREATE OR REPLACE VIEW CampingsPalavas (idCamping, nomCamping, villeCamping, nbEtoilesCamping) AS
+	SELECT idCamping, nomCamping, villeCamping, nbEtoilesCamping
+	FROM Campings
+	WHERE villeCamping = 'Palavas'
+	WITH CHECK OPTION;
+
+	INSERT INTO CampingsPalavas VALUES ('CAMP5', 'CamCamPing', 'Montpellier', 3);
+	INSERT INTO CampingsPalavas VALUES ('CAMP5', 'CamCamPing', 'Palavas', 3);
+
+--Modification de la vue pour qu'elle soit non modifiable
+
+CREATE OR REPLACE VIEW CampingsPalavas (idCamping, nomCamping, villeCamping, nbEtoilesCamping) AS
+	SELECT idCamping, nomCamping, villeCamping, nbEtoilesCamping
+	FROM Campings
+	WHERE villeCamping = 'Palavas'
+	WITH READ ONLY;
+
+--8
+
+--
+
+GRANT SELECT
+ON Clients
+TO PUBLIC;
+
+SELECT * FROM abatm.Clients;
+
+INSERT INTO Clients VALUES ('C3PO', 'Chie', 'Jean', '14/08/1995', 'Paris');
+
+CREATE SYNONYM ClientsDeMael FOR abatm.Clients;
+
+REVOKE SELECT ON Clients FROM PUBLIC;
+
+CREATE VIEW ClientsMtp AS
+	SELECT *
+	FROM Clients
+	WHERE villeClient = 'Montpellier';
+
+GRANT SELECT
+ON ClientsMtp
+TO abatm;
+
+SELECT * FROM abatm.ClientsMtp;
+
+GRANT INSERT
+ON ClientsMtp
+to abatm;
+
+INSERT INTO abatm.Clients VALUES ('TUPL', 'Sinsere', 'DansMael', '18/03/1999', 'Montpellier');
+
+--9
+
+CREATE VIEW LocationsJuillet (idLocation, idClient, nomClient, prenomClient, idBungalow, nomBungalow, nomCamping, dateDebut, dateFin, montantLocation) AS
+	SELECT idLocation, l.idClient, nomClient, prenomClient, l.idBungalow, nomBungalow, nomCamping, dateDebut, dateFin, montantLocation
+	FROM Locations l
+	JOIN Bungalows b ON l.idBungalow = b.idBungalow
+	JOIN Clients c ON l.idClient = c.idClient
+	JOIN Campings c ON b.idCamping = c.idCamping
+		WHERE dateDebut <= '31/07/2021'
+		AND dateFin >= '01/07/2021';
+
+GRANT SELECT
+ON LocationsJuillet
+to abatm;
+
+GRANT INSERT
+ON LocationsJuillet
+to abatm;
+
+
+INSERT INTO abatm.LocationsJuillet VALUES ('L53', 'C20', 'Konté', 'Bakari', 'B974', '46K Sport', 'Kemar', '14/08/2021', '28/08/2021', 2000);
+
+-- Impossible d'insérer car table mutlivue
+
+--Partie 4
+
+--R22
+
+CREATE VIEW EmployesLFB (idEmploye, nomEmploye, prenomEmploye, salaireEmploye) AS
+	SELECT idEmploye, nomEmploye, prenomEmploye, salaireEmploye
+	FROM Employes e
+	JOIN Campings c ON e.idCamping = c.idCamping
+	WHERE nomCamping = 'Les Flots Bleus';
+
+
+SELECT *
+FROM EmployesLFB
+WHERE salaireEmploye = (SELECT MIN(salaireEmploye)FROM EmployesLFB);
+
+--Requete à l'origine
+
+SELECT nomEmploye, prenomEmploye 
+	FROM Employes e
+	JOIN Campings camp ON e.idCamping = camp.idCamping
+	WHERE nomCamping ='Les Flots Bleus'
+		AND salaireEmploye = (SELECT MIN(salaireEmploye) FROM Employes e
+									JOIN Campings camp ON e.idCamping = camp.idCamping
+										WHERE nomCamping = 'Les Flots Bleus');
+
+--R87
+
+CREATE VIEW EmployesParCamping (nomCamping, nbEmployes) AS
+	SELECT nomCamping, COUNT(idEmploye)
+	FROM Employes e
+	JOIN Campings c ON e.idCamping = c.idCamping
+	GROUP BY nomCamping;
+
+
+
+SELECT nomCamping
+FROM EmployesParCamping
+WHERE nbEmployes = (SELECT MAX(COUNT(nbEmployes))
+GROUP BY nomCamping;
+
+--Requete a l'origine
+
+SELECT nomCamping
+FROM Campings c
+JOIN Employes e ON e.idCamping = c.idCamping
+GROUP BY c.idCamping, nomCamping
+HAVING COUNT(*) = (SELECT MAX(COUNT(*)
+FROM Employes
+GROUP BY idCamping);
+
+
+
+
+
+INSERT INTO abatm.LocationsJuillet VALUES ('L69', 'S3XE', 'Siffredi', 'Rocco', '8IID', 'Brazzers', 'Le Cheval Bien Monté', '14/07/2021', '28/07/2021', 1000);
